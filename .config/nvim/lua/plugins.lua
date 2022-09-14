82af68f1cfb2a265d8d2d435601dc50823d5bf2d
Flawweng <43078425+Flawweng@users.noreply.github.com>
2022-09-14 21:46:17 +0200

    Add comments

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -20,19 +20,19 @@ packer.startup(function(use)
   use 'hrsh7th/nvim-cmp' -- Completion
   use 'neovim/nvim-lspconfig' -- LSP
   use 'glepnir/lspsaga.nvim' -- LSP UIs
-  use 'L3MON4D3/LuaSnip'
-  use 'saadparwaiz1/cmp_luasnip'
-  use "rafamadriz/friendly-snippets"
+  use 'L3MON4D3/LuaSnip' -- Snippet engine
+  use 'saadparwaiz1/cmp_luasnip' -- Snippet engine cmp
+  use "rafamadriz/friendly-snippets" -- Snippets
   use {
     'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate'
   }
   use 'kyazdani42/nvim-web-devicons' -- File icons
-  use 'nvim-telescope/telescope.nvim'
-  use 'nvim-telescope/telescope-file-browser.nvim'
+  use 'nvim-telescope/telescope.nvim' -- File explorer
+  use 'nvim-telescope/telescope-file-browser.nvim' -- File explorer
   use 'windwp/nvim-autopairs'
   use 'windwp/nvim-ts-autotag'
-  use 'akinsho/nvim-bufferline.lua'
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
+  use 'akinsho/nvim-bufferline.lua'    
+  use 'williamboman/mason.nvim' -- Package manager
+  use 'williamboman/mason-lspconfig.nvim'-- Package manager for LSP
 end)
